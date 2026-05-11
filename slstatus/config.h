bf7acc628fa9fb01c99119e0d8a7212bb51ffaac
Kenneth Adjetey Adjei-mensah <kenadjei20@gmail.com>
2026-05-11 10:02:57 +0000

    update of panel to be dynamic when it comes to battery charging state and wifi state

--- slstatus/config.h
+++ slstatus/config.h
@@ -23,11 +23,16 @@ static const char unknown_str[] = "n/a";
  */
 
 static const struct arg args[] = {
-    /* function         format                argument */
-    { wifi_perc,       " %s%% | ",          "wlo1" },
-    { battery_perc,    " %s%% | ",          "BAT1" },
-    { battery_state,   "%s | ",              "BAT1" },
-    { ram_perc,        " %s%% | ",          NULL },
-    { cpu_perc,        " %s%% | ",          NULL },
-    { datetime,        " %s",               "%a %d %b %Y, %r" },
+    /* Network: USB / WiFi signal / Offline */
+    { run_command,  "%s | ",              "if ip route get 1.1.1.1 2>/dev/null | grep -q 'enx'; then echo '󰁆 USB'; elif grep -q 'up' /sys/class/net/wlo1/operstate 2>/dev/null; then q=$(cat /proc/net/wireless 2>/dev/null | awk 'NR==3 {print int($4*100/70)}'); if [ $q -ge 80 ]; then echo '󰤨 WiFi' $q'%'; elif [ $q -ge 60 ]; then echo '󰤥 WiFi' $q'%'; elif [ $q -ge 40 ]; then echo '󰤢 WiFi' $q'%'; elif [ $q -ge 20 ]; then echo '󰤟 WiFi' $q'%'; else echo '󰤭 WiFi' $q'%'; fi; else echo '󰤭 Offline'; fi" },
+
+    /* Battery: Shows different icons based on charge AND state (ternary chain) */
+    { run_command,  "%s | ",              "s=$(cat /sys/class/power_supply/BAT1/status); p=$(cat /sys/class/power_supply/BAT1/capacity); if [ \"$s\" = \"Charging\" ]; then echo \"󰂄 $p%\"; else case $p in 9[0-9]|100) echo \"󰁹 $p%\";; 7[0-9]|8[0-9]) echo \"󰂀 $p%\";; 5[0-9]|6[0-9]) echo \"󰁾 $p%\";; 3[0-9]|4[0-9]) echo \"󰁼 $p%\";; 1[0-9]|2[0-9]) echo \"󰁺 $p%\";; *) echo \"󰂎 $p%\";; esac; fi" },
+
+    /* System */
+    { ram_perc,      "󰍛 %s%% | ",         NULL },
+    { cpu_perc,      "󰻠 %s%% | ",         NULL },
+
+    /* Time */
+    { datetime,      "󰅐 %s",              "%a %d %b %Y, %I:%M %p" },
 };
