b3a08e5d5729745fce981ccefe9a09968d0ae252
Kenneth Adjetey Adjei-mensah <kenadjei20@gmail.com>
2026-05-12 10:27:11 +0000

    Complete processes and signals project with all scripts and README

--- dwm/config.h
+++ dwm/config.h
@@ -7,6 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int splitstatus        = 1;        /* 1 for split status items */
+static const int smartgaps          = 0;
 static const char *splitdelim        = ";";       /* Character used for separating status */
 static const char *fonts[]          = { "Hack:size=12", "Symbols Nerd Font:size=12" };
 static const char dmenufont[]       = "size=12";
